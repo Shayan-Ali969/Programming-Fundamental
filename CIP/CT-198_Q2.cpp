f3bba1fe328c6710d1a2d8ad1142b5b2093c5dcc
Shayan-Ali969 <shayannnn568@gmail.com>
2025-10-23 20:23:13 +0500

    Add files via upload

---
+++ CIP/CT-198_Q2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+      float Temperature;
+      printf("Temperature :");
+      scanf("%f",&Temperature);
+      
+      if(Temperature>30)
+      printf("Weather is Hot");
+      
+      else
+      printf("Weather is Cold");
+}
