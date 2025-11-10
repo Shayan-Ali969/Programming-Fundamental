fb2af3c85aeeaeef75f1f636f5795546fcba7a78
Shayan-Ali969 <shayannnn568@gmail.com>
2025-11-11 01:16:49 +0500

    Add files via upload

---
+++ CIP/Question 2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int cube(int x)
+	{
+   		return x * x * x; 
+	}
+
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    
+    scanf("%d", &num);
+    printf("The cube of %d is %d\n", num, cube(num));
+    
+    return 0;
+}
---
+++ CIP/Question 3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+	{
+   		int temp;
+    	temp = *a;   
+    	*a = *b;     
+    	*b = temp;   
+	}
+
+int main()
+{
+    int x = 15, y = 25;
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swapping:  x = %d, y = %d\n", x, y);
+
+    return 0;
+}
