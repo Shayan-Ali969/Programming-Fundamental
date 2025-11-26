117cd7c2c11f44b6b89c0d08f275fccc67657bdd
Shayan-Ali969 <shayannnn568@gmail.com>
2025-11-27 02:07:11 +0500

    Add files via upload

---
+++ Practice/triplet_array.cpp
@@ -0,0 +1,23 @@
+#include<Stdio.h>
+int main()
+{
+	int arr[8]={1,2,3,4,5,6,7,8};
+	int x=12;
+	int totalpairs=0;
+	
+	for(int i=0;i<=7;i++)
+	{
+		for(int j=i+1;j<=7;j++)
+		{
+			for(int k=j+1;k<=7;k++)
+			{
+			if(arr[i]+arr[j]+arr[k]==12)
+			{
+			totalpairs++;
+			printf("(%d,%d,%d)\n",arr[i],arr[j],arr[k]);
+			}
+			}
+		}
+	}
+	printf("total pairs are %d",totalpairs);
+}
