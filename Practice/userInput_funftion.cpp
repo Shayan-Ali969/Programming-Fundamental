ba2a50a7e230d30f8ad6107e28befc67080f839c
Shayan-Ali969 <shayannnn568@gmail.com>
2025-11-28 18:14:22 +0500

    Add files via upload

---
+++ Practice/userInput_funftion.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int add(int a,int b);
+
+int main()
+{
+	int num1,num2,result;
+	printf("Enter first number :");
+	scanf("%d",&num1);
+	
+	printf("Enter first number :");
+	scanf("%d",&num2);
+	
+	result=add(num1,num2);
+	printf("Sum is %d",result);
+	
+	
+}
+
+int add(int a,int b)
+{
+	return a+b;
+}
